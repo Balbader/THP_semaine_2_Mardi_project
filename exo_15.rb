6c6e81b7017e24fc356a0241d7ff143f12a63414
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-12 17:22:55 +0100

    Update

--- exo_15.rb
+++ exo_15.rb
@@ -1,7 +1,12 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu"
+puts "Salut, bienvenue dans ma super pyramide ! Entre un numero entre 1 et 25:"
 print "> "
 n = gets.chomp.to_i
 
+if n > 25
+  puts "Error!!!!!!!!!!!!"
+else
 for i in 1..n
   puts "#{Array.new(i, "#").join('')}  #{Array.new(n - i, " ").join('')}"
+  sleep(0.1)
+end
 end
