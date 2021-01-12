5fff28fec6678d8fcb5a725f47deab4c7ea11810
Balbader <15971200+Balbader@users.noreply.github.com>
2021-01-12 19:03:32 +0100

    Initial commit

--- exo_17.rb
+++ exo_17.rb
@@ -1,16 +1,20 @@
 puts "Salut, bienvenue dans ma super pyramide ! Entre un numero entre 1 et 25 ?"
 print "> "
 nb = gets.chomp.to_i
-puts "Voici la pyramide :"
 
 i = 0
 j = 0
 char = "#"
-print " " * (nb - nb / 2)
-while nb >= i do
-  print " " * (nb / 2)
-  puts "#{char}" * (2 * j + 1)
-  print " " * (nb / 2)
-  nb -= 1
-  j += 1
+if nb > 25
+  puts "Error!"
+else
+  puts "Voici la pyramide :"
+  print " " * (nb - nb / 2)
+  while nb >= i do
+    print " " * (nb / 2)
+    puts "#{char}" * (2 * j + 1)
+    print " " * (nb / 2)
+    nb -= 1
+    j += 1
+  end
 end
