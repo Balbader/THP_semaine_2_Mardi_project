6fb293da08a1654f5dd561143491e43a67d65deb
Charles <charlescampet03@gmail.com>
2021-01-12 17:10:44 +0100

    update commit

--- exo_15.rb
+++ exo_15.rb
@@ -1,7 +1,11 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu"
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu compris entre 1 et 25 ?"
 print "> "
-n = gets.chomp.to_i
+n = gets.to_i
 
-for i in 1..n
-  puts "#{Array.new(i, "#").join('')}  #{Array.new(n - i, " ").join('')}"
+if n < 1 || n > 25
+    print "Erreur, met des étages entre 1 et 25 !"
+else
+    for i in 1..n
+    puts "#{Array.new(i, "#").join('')}  #{Array.new(n - i, " ").join('')}"
+    end
 end
